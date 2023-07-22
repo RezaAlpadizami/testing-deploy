f4ea50d6a27afd0233277f6511deede51eb7e3e7
Reza Alpadizami <alpdizami94@gmail.com>
2023-07-23 03:38:54 +0700

    deploy vercel

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const PORT = process.env.PORT || 5000
 const express = require("express");
 // const bodyParser = require("body-parser");
 const cors = require("cors");
-const guestRoutes = require("./routes/guest");
-const guestNameRoutes = require("./routes/guestName")
+const guestRoutes = require("./src/routes/guest");
+const guestNameRoutes = require("./src/routes/guestName")
 
-const middlewareLogRequest = require('./middleware/logs')
+const middlewareLogRequest = require('./src/middleware/logs')
 
 const app = express();
 
