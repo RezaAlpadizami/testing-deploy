1882cff79fd64b4281c4fedf3b72f1b89cafdeab
Reza Alpadizami <alpdizami94@gmail.com>
2023-07-27 03:51:06 +0700

    deploy vercel

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const express = require("express");
-const cacheControl = require('cache-control');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 // const bodyParser = require("body-parser");
+const compression = require('compression');
 const cors = require("cors");
 const guestRoutes = require("./src/routes/guest");
 const guestNameRoutes = require("./src/routes/guestName")
@@ -10,13 +12,11 @@ const guestNameRoutes = require("./src/routes/guestName")
 const middlewareLogRequest = require('./src/middleware/logs')
 
 const app = express();
+app.use(compression());
 
-app.use(cacheControl({
-  maxAge: "1d",
-  public: true,
-}));
+app.use(cookieParser());
 
-app.use(express.static('public'));
+app.use(express.static('public', { gzip: true }));
 
 app.use(cors());
 app.use(middlewareLogRequest);
@@ -24,6 +24,15 @@ app.use(express.json());
 
 app.use("/guest", guestRoutes);
 app.use("/guestname", guestNameRoutes);
+
+app.get("/", (req, res) => {
+  // Set a cookie with SameSite=None and Secure attributes
+  res.cookie({
+    sameSite: "none",
+    secure: true,
+});
+  res.send("Cookie set successfully!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
